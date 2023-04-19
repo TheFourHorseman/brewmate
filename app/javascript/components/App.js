420b4df0759cd3e2a779632b5b78404a1c919889
XavierB91 <xavierbarker183@gmail.com>
2023-04-19 09:58:02 -0700

    intial commit

---
+++ app/javascript/components/App.js
@@ -0,0 +1,12 @@
+import React from "react"
+import PropTypes from "prop-types"
+import { BrowserRouter, Routes,Route } from "react-router-dom";
+
+const App = () => {
+  return (
+    <div>App</div>
+  )
+}
+
+export default App
+
