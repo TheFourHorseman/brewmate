0528c02620afc0a38ae8af6c8cbf81549615140f
alexander.joey@gmail.com <learnacademy@LEARNs-MacBook-Air.local>
2023-04-20 12:04:44 -0700

    added navbar with all navigation for logged in users and users not logged in

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -9,11 +9,14 @@ import BeerNew from "./pages/BeerNew";
 import BeerSuggestions from "./pages/BeerSuggestions";
 import BeerProfile from "./pages/BeerProfile";
 import MyBeers from "./pages/MyBeers";
+import Header from "./components/Header"
 
 const App = (props) => {
+  console.log(props)
   return (
     <>
       <BrowserRouter>
+      <Header {...props}/>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/beerindex" element={<BeerIndex />} />
--- app/javascript/components/components/Header.js
+++ app/javascript/components/components/Header.js
@@ -1,9 +1,66 @@
-import React from 'react'
+import React, { useState } from 'react';
+import {
+  Nav,
+  NavItem,
+  Dropdown,
+  DropdownItem,
+  DropdownToggle,
+  DropdownMenu,
+  NavLink,
+} from 'reactstrap';
 
-const Header = () => {
+const Header = ({
+  logged_in,
+  current_user,
+  new_user_route,
+  sign_in_route,
+  sign_out_route
+}) => {
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+
+  const toggle = () => setDropdownOpen(!dropdownOpen);
+
   return (
-    <div>Header</div>
-  )
+    <div>
+  <Nav tabs>
+    <NavItem>
+      <NavLink href="/" active>
+        Brewmate
+      </NavLink>
+    </NavItem>
+    <NavItem>
+      <NavLink href="/beerindex">All Beers</NavLink>
+    </NavItem>
+    {logged_in && (
+      <>
+        <Dropdown nav isOpen={dropdownOpen} toggle={toggle}>
+          <DropdownToggle nav caret>
+            {current_user.email}
+          </DropdownToggle>
+          <DropdownMenu>
+            <DropdownItem href='/beerprofile'>Profile</DropdownItem>
+            <DropdownItem href='/beershow'>Your Beers</DropdownItem>
+            <DropdownItem>Liked Beers</DropdownItem>
+            <DropdownItem href='/beernew'>Add Beer</DropdownItem>
+            <DropdownItem divider /> 
+            <DropdownItem href={sign_out_route}>Log out</DropdownItem>
+          </DropdownMenu>
+        </Dropdown>
+      </>
+    )}
+    {!logged_in && (
+      <>
+        <NavItem>
+          <NavLink href={sign_in_route}>Log in</NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink href={new_user_route}>Create an account</NavLink>
+        </NavItem>
+      </>
+    )}
+  </Nav>
+</div>
+  );
 }
 
 export default Header
