ef6da6526dacb479d98a4ea189b132d2d553989d
Aaron Criner <aaron.criner@me.com>
2023-04-20 11:58:29 -0700

    completed testing for index

--- app/javascript/components/pages/BeerIndex.js
+++ app/javascript/components/pages/BeerIndex.js
@@ -11,29 +11,31 @@ import {
 const BeerIndex = ({ beers }) => {
   return (
     <>
-      <h2>View All Brews</h2>
       <div className="index-content">
-        {beers.map((beer, index) => {
-          return (
-            <Card
-              style={{
-                width: "18rem",
-              }}
-              key={index}
-              className="index-card"
-            >
-              <img alt="Sample" src="https://picsum.photos/300/200" />
-              <CardBody>
-                <CardTitle tag="h5">{beer.beer_name}</CardTitle>
-                <CardSubtitle className="mb-2 text-muted" tag="h6">
-                  {beer.brewery_name}
-                </CardSubtitle>
-                <CardText>{beer.beer_style}</CardText>
+        <h2>View All Brews</h2>
+        <div className="index-cards">
+          {beers.map((beer, index) => {
+            return (
+              <Card
+                style={{
+                  width: "18rem",
+                }}
+                key={index}
+                className="index-card"
+              >
+                <img alt="Sample" src="https://picsum.photos/300/200" />
+                <CardBody>
+                  <CardTitle tag="h5">{beer.beer_name}</CardTitle>
+                  <CardSubtitle className="mb-2 text-muted" tag="h6">
+                    {beer.brewery_name}
+                  </CardSubtitle>
+                  <CardText>{beer.beer_style}</CardText>
+                </CardBody>
                 <Button>Show More</Button>
-              </CardBody>
-            </Card>
-          );
-        })}
+              </Card>
+            );
+          })}
+        </div>
       </div>
     </>
   );
