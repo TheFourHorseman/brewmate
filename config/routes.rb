f86914bf5fafd5c774cf20923d3a348ac53ab13b
Aaron Criner <aaron.criner@me.com>
2023-04-25 16:00:12 -0700

    added readme and prepped for deployment on render

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   resources :beers
   resources :likes
-  devise_for :users
   resources :users do
     get 'suggested_beers', to: 'beers#suggested_beers'
   end
