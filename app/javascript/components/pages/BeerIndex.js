c0132e4653a74f945ea6285f960ce07129054355
Aaron Criner <aaron.criner@me.com>
2023-04-27 11:49:46 -0700

    styling changes

--- app/javascript/components/pages/BeerIndex.js
+++ app/javascript/components/pages/BeerIndex.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import {
   Card,
   CardBody,
@@ -10,50 +10,63 @@ import {
 import { NavLink, useNavigate } from "react-router-dom";
 import IndexCard from "../components/IndexCard";
 
-
-const BeerIndex = ({beers,
+const BeerIndex = ({
+  beers,
   current_user,
   likeBeer,
   deleteLike,
   deleteBeer,
   likes,
-  logged_in,}) => {
-  
-  const [search, setSearch] = useState('');
-  const [response, setResponse] =  useState()
-  const navigate = useNavigate()
+  logged_in,
+}) => {
+  const [search, setSearch] = useState("");
+  const [response, setResponse] = useState();
+  const navigate = useNavigate();
 
   function handleInputChange(event) {
     setSearch(event.target.value);
-    searchResponse()
+    searchResponse();
   }
-
 
   const filteredBeers = beers?.filter((beer) =>
     beer?.beer_name.toLowerCase().includes(search.toLowerCase())
   );
-  
+
   const searchResponse = () => {
-      if(logged_in === true && filteredBeers.length === 0){
-        setResponse(1)
-      }else if (filteredBeers.length === 0){
-        setResponse(2)
-      }else {setResponse(3)}
-  }
+    if (logged_in === true && filteredBeers.length === 0) {
+      setResponse(1);
+    } else if (filteredBeers.length === 0) {
+      setResponse(2);
+    } else {
+      setResponse(3);
+    }
+  };
 
   return (
     <>
-    <div>
-      <input type="text" placeholder="Search by beer name..." value={search} onChange={handleInputChange} />
-      {response === 1 && (
-          <h2>{`The beer: "${search}" was not found. You can add it `}<NavLink to="/beernew">here</NavLink></h2>
-      )}
-      {response === 2 && (
-          <h2>{`The beer: "${search}" was not found.`}</h2>
-      )}
-      <div className="index-cards">
-        {filteredBeers?.map((beer, index) => {
-         return (
+      <div>
+        <input
+          type="text"
+          placeholder="Search by beer name..."
+          value={search}
+          onChange={handleInputChange}
+        />
+        {response === 1 && (
+          <>
+            <h2>
+              {`The beer: "${search}" was not found.`}
+              <h2>
+                <NavLink to="/beernew">
+                  <strong>You can add it here</strong>
+                </NavLink>
+              </h2>
+            </h2>
+          </>
+        )}
+        {response === 2 && <h2>{`The beer: "${search}" was not found.`}</h2>}
+        <div className="index-cards">
+          {filteredBeers?.map((beer, index) => {
+            return (
               <IndexCard
                 beer={beer}
                 key={index}
@@ -66,11 +79,11 @@ const BeerIndex = ({beers,
                 likes={likes}
               />
             );
-        })}
+          })}
         </div>
-    </div>
+      </div>
     </>
   );
-}
+};
 
 export default BeerIndex;
